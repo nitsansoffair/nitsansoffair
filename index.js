fb460517c096dd430903044126fea21ed8a60f4e
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-05-29 15:34:50 +0300

    Deploy - Node.js server

---
+++ index.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
