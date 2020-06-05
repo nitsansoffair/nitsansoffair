6ac89922a203124049792dcbea4c0f01866f8336
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-06-05 20:50:03 +0300

    Contact Form - jQuery, Description

--- jquery/index.js
+++ jquery/index.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
     $(".navigation__link").click(function() {
         $("#navi-toggle").prop("checked", false);
     });
+
+    $('#submit').click(function (e) {
+        e.preventDefault();
+    });
 });
